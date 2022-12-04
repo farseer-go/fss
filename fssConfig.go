128fee22a98b7dadabfe6bde9ba56d9b27f61cf3
steden <1470804@qq.com>
2022-12-04 16:10:45 +0800

    完成：fss 分布式调度中心客户端文档

--- fssConfig.go
+++ fssConfig.go
@@ -6,9 +6,10 @@ import (
 )
 
 type fssConfig struct {
-	Server    string // fss服务端地址
-	PullCount int    // 客户端每次拉取数量
-	WorkCount int    // 允许最大并行执行的任务数量
+	Server       string // fss服务端地址
+	PullCount    int    // 客户端每次拉取数量
+	WorkCount    int    // 允许最大并行执行的任务数量
+	PullInterval int    // 客户端每次拉取的时间间隔（ms）
 }
 
 // 允许最大并行执行的任务数量
